759b6d14897fcac6b2a2fd6153b3fcfb06d3ae62
Shelan <shelandebesai@gmail.com>
2019-12-17 15:01:43 -0800

    Update Iterative-inorder-traversal.js

--- Iterative-inorder-traversal.js
+++ Iterative-inorder-traversal.js
@@ -1,1 +1,21 @@
-function inorderTraversal()
+function inorderTraversal(root) {
+  let stack = []
+  
+  if(!root) return stack 
+ 
+  let result = []
+  let curr = root
+  
+  while(stack.length > 0 || current != null) {
+    if(current) {
+      stack.push(current)
+      current = current.left 
+    } else {
+      current = stack.pop()
+      result.push(current.val)
+      current = current.right
+    }
+  }
+  return result
+  
+}
