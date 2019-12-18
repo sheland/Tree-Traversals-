41e375044c620417d4891a07f1210c2d9a55c714
Shelan <shelandebesai@gmail.com>
2019-12-17 17:16:30 -0800

    Update recursive-preorder-traversal.js

--- recursive-preorder-traversal.js
+++ recursive-preorder-traversal.js
@@ -1,13 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
 function preOrder() {
   let result = []
   preOrderHelper(root);
 }
 //Helper function: 
 
-function preOrderHelper(root) {
-   if (root !== null) {
+  function preOrderHelper(root) {
+    if (root !== null) {
       result.push(root.val);
       preOrderHelper(root.left);
       preOrderHelper(root.right);
    }
+  }
+return result
 }
