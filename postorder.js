617620ed21d25d3b4365feecb4e69533de093d4d
Shelan <shelandebesai@gmail.com>
2019-12-17 16:30:42 -0800

    Create postorder.js

---
+++ postorder.js
@@ -0,0 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ 
+ 
