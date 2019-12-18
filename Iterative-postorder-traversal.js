cbe07838c71d27c6f714d7957a9bc2efba949471
Shelan <shelandebesai@gmail.com>
2019-12-17 16:39:10 -0800

    Update Iterative-postorder-traversal.js

--- Iterative-postorder-traversal.js
+++ Iterative-postorder-traversal.js
@@ -10,4 +10,27 @@
  * @return {number[]}
  */
  
- 
+ function postOrder(root) {
+  
+  if(!root) return []
+  
+  let stack = [root]
+  let result = []
+  
+  while(stack.length > 0) {
+   let curr = stack.pop()
+   result.unshift(curr.val)
+   
+   if(curr.left) {
+    stack.push(curr.right)
+   }
+   
+   if(curr.right) {
+    stack.push(curr.left)
+   }
+   
+  }
+  return result
+  
+  
+ }
