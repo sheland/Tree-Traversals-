95a17d63fdd744c88de3ddb201def66f6e57b04d
Shelan <shelandebesai@gmail.com>
2019-12-17 16:40:04 -0800

    Update Iterative-inorder-traversal.js

--- Iterative-inorder-traversal.js
+++ Iterative-inorder-traversal.js
@@ -1,3 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+
 function inorderTraversal(root) {
   let stack = []
   
